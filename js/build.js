bbf717516a9df0df0356afffac0c3689f14a9acc
Sidharth Baskaran <sidnbaskaran@gmail.com>
2021-08-15 00:04:25 -0500

    Add files

--- js/build.js
+++ js/build.js
@@ -1,2 +1,12 @@
-// bunch of stuff todo
-console.log('bruh bruh bruh')
+
+// get markdown JSON header and parse
+
+// get markdown file content
+
+//convert markdown -> HTML
+
+// inject markdown into HTML post template
+
+// save built HTML file into posts
+
+// update blog page with links
--- js/darkmode.js
+++ js/darkmode.js
@@ -1,11 +1,23 @@
-// toggle dark mode
 function toggleDarkMode() {
-    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    //localStorage.theme === 'dark' || (!('theme' in localStorage) && matchMedia('(prefers-color-scheme: dark)').matches)
+    localStorage.setItem('theme', localStorage.getItem('theme') === 'light' ? 'dark' : 'light')
+
+    if (localStorage.getItem('theme') === 'dark') {
         document.documentElement.classList.add('dark')
     } else {
         document.documentElement.classList.remove('dark')
     }
-    // Whenever the user explicitly chooses light mode
-    localStorage.theme = localStorage.theme === 'light' ? 'dark' : 'light'
+
+    var icon = document.getElementById('sunmoon')
+    var back = document.getElementById('backbutton')
+
+    icon.src = localStorage.theme === 'light' ? '/img/icons/light-mode.svg' : '/img/icons/dark-mode.svg'
+    back.src = localStorage.theme === 'light' ? '/img/icons/navigate-light.svg' : '/img/icons/navigate-light.svg'
+}
+
+//on page load
+if (localStorage.getItem('theme') === 'dark') {
+    document.documentElement.classList.add('dark')
+} else {
+    document.documentElement.classList.remove('dark')
 }
