3f9aa235ae69f387c2b3fc96809fc5f972451513
Sidharth Baskaran <sidnbaskaran@gmail.com>
2022-01-14 07:49:34 -0600

    Add documentation

--- js/darkmode.js
+++ js/darkmode.js
@@ -19,5 +19,5 @@ function toggleDarkMode() {
 if (localStorage.getItem('theme') === 'dark') {
     document.documentElement.classList.add('dark')
 } else {
-    document.documentElement.classList.remove('dark')
+     document.documentElement.classList.remove('dark')
 }
