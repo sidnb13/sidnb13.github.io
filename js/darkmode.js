91558892ae20da43ed9fb4c7fe201adf3bd4050b
Sidharth Baskaran <sidnbaskaran@gmail.com>
2022-03-16 16:13:35 -0500

    Fix darkmode, streamline

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,23 +1,16 @@
 function toggleDarkMode() {
-    //localStorage.theme === 'dark' || (!('theme' in localStorage) && matchMedia('(prefers-color-scheme: dark)').matches)
-    localStorage.setItem('theme', localStorage.getItem('theme') === 'light' ? 'dark' : 'light')
-
     if (localStorage.getItem('theme') === 'dark') {
-        document.documentElement.classList.add('dark')
-    } else {
         document.documentElement.classList.remove('dark')
+        localStorage.setItem('theme', 'light')
+    } else {
+        document.documentElement.classList.add('dark')
+        localStorage.setItem('theme', 'dark')
     }
-
-    var icon = document.getElementById('sunmoon')
-    var back = document.getElementById('backbutton')
-
-    icon.src = localStorage.theme === 'light' ? '/img/icons/light-mode.svg' : '/img/icons/dark-mode.svg'
-    back.src = localStorage.theme === 'light' ? '/img/icons/navigate-light.svg' : '/img/icons/navigate-light.svg'
 }
 
-//on page load
-if (localStorage.getItem('theme') === 'dark') {
+// on load action
+
+if (localStorage.getItem('theme') === 'dark')
     document.documentElement.classList.add('dark')
-} else {
-     document.documentElement.classList.remove('dark')
-}
+else
+    document.documentElement.classList.remove('dark')
